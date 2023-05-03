e1b4378a21ea5829d7fe7c04d4cc406a2e0a02ae
kacper <kacpersloka@o2.pl>
2023-05-03 16:20:39 +0200

    stuff

--- src/components/ProgressItem/ProgressItem.js
+++ src/components/ProgressItem/ProgressItem.js
@@ -5,9 +5,10 @@ import PropTypes from 'prop-types';
 import Button from '../Button/Button';
 
 import { ReactComponent as FullArrowSVG } from '../../assets/icons/full-arrow.svg';
+import { ReactComponent as LinesSVG } from './assets/lines.svg';
 import { useScrollContext } from "../../context/Scroll";
 
-function ProgressItem({ heading, text, href, src }) {
+function ProgressItem({ heading, text, href, src, type = 'normal', children }) {
   const [isActive, setIsActive] = useState(false);
   const containerRef = useRef();
   const { scrollY } = useScrollContext();
@@ -23,30 +24,67 @@ function ProgressItem({ heading, text, href, src }) {
       setIsActive(false);
     }
   }, [scrollY]);
-  
+
   return (
-    <div className={css['container']} ref={containerRef}>
+    <div 
+      className={`
+        ${css['container']}
+        ${type !== 'normal' && css[`container--${type}`]}
+        ${isActive && css['container--active']}
+      `} 
+      ref={containerRef}
+    >
       <div 
-        className={`${css['bar']} ${isActive && css['bar--active']}`}
+        className={`${css['bar']}`}
       >
-        <div className={css['bar__circle']}/>
+        <div className={css['circle']}/>
+        {type === 'break' && <div className={css['fog']}/>}
       </div>
+
+      {
+        type === 'special' && 
+          <div className={css['line-container']}>
+            <LinesSVG 
+              className={`
+                ${css['line']}
+                ${isActive && 'super-stroke-container--active'}
+              `}
+            />
+            <div className={css['circle']}/>
+          </div>
+      }
       
-      <article className={css['content']}>
-        <h3 className="head head--c">{heading}</h3>
-        <p className="para para--a">{text}</p>
-        {href && <Button type="link link-b" href="https://www.cypress.io/app">Learn more <FullArrowSVG/></Button>}
-        {src && <img className={css['image']} src={src}/>}
-      </article>
+      {
+        children ?
+          children
+        :
+          <article className={css['content']}>
+            {
+              Array.isArray(src) && 
+                <div className={css['icon-container']}>
+                  {src.map(src => <img key={src} className={css['icon']} src={src}/>)}
+                </div>
+            }
+            <h3 className={`head head--c ${type === 'special' && 'head--color-b'}`}>{heading}</h3>
+            <p className="para para--a">{text}</p>
+            {href && <Button type="link link-b" href={href}>Learn more <FullArrowSVG/></Button>}
+            {typeof src === 'string' && <img className={css['image']} src={src}/>}
+          </article>
+      }
     </div>
   );
 }
 
 ProgressItem.propTypes = {
-  heading: PropTypes.string.isRequired,
-  text: PropTypes.string.isRequired,
+  heading: PropTypes.string,
+  text: PropTypes.string,
   href: PropTypes.string,
-  src: PropTypes.string,
+  src: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.arrayOf(PropTypes.string),
+  ]),
+  type: PropTypes.oneOf(['normal', 'special', 'break']),
+  children: PropTypes.node,
 };
 
 export default ProgressItem;
--- src/components/WriteRunDebug/WriteRunDebug.js
+++ src/components/WriteRunDebug/WriteRunDebug.js
@@ -4,50 +4,102 @@ import css from './WriteRunDebug.module.css';
 import ProgressItem from '../ProgressItem/ProgressItem';
 
 import { ReactComponent as LaptopSVG } from './assets/laptop.svg';
+import { ReactComponent as ClockSVG } from './assets/clock.svg';
 import debugSrc from './assets/debug.png';
 import eliminateSrc from './assets/eliminate.png';
-import gainSrc from './assets/gain.png';
+import gainSrc from './assets/gain.svg';
 import integrateSrc from './assets/integrate.png';
 import optimizeSrc from './assets/optimize.png';
-import reviewSrc from './assets/review.png';
+import reviewSrc from './assets/review.svg';
 import testSrc from './assets/test.png';
 import writeSrc from './assets/write.png';
+import int_1 from './assets/int-1.svg';
+import int_2 from './assets/int-2.svg';
+import int_3 from './assets/int-3.svg';
+import int_4 from './assets/int-4.svg';
+import int_5 from './assets/int-5.svg';
+import int_6 from './assets/int-6.svg';
 
 function WriteRunDebug() {
   return (
     <div className={css['container']}>
       <div className="max-width">
-        <div className={css['top']}>
-          <LaptopSVG style={{ color: 'rgb(90, 95, 122)', display: 'block', margin: 'auto' }}/>
+        <div className={css['heading']}>
+          <LaptopSVG className={css['heading__icon']}/>
           <p className="para para--b para--medium">Introducing the Cypress App</p>
           <h2 className="head head--b head--medium">Write, run, and debug <span className="grad-text grad-text--a">like a pro</span></h2>
         </div>
 
         <div style={{ padding: 'clamp(32px, 5vw, 64px) 0' }}>
           <ProgressItem
-            heading={'Test modern apps directly in the browser'}
-            text={'Watch your end-to-end and component tests run in real-time as you develop your applications. Cypress’s simple, yet powerful API runs as fast as your browser can render content.'}
-            href={'https://www.cypress.io/app'}
+            heading="Test modern apps directly in the browser"
+            text="Watch your end-to-end and component tests run in real-time as you develop your applications. Cypress’s simple, yet powerful API runs as fast as your browser can render content."
+            href="https://www.cypress.io/app"
             src={testSrc}
           />
           <ProgressItem
-            heading={'Write your first passing test in minutes'}
-            text={'Installing Cypress and writing your first passing test is a breeze. There are no servers, drivers, or other dependencies to install or configure.'}
-            href={'https://www.cypress.io/app#delightful_experience'}
+            heading="Write your first passing test in minutes"
+            text="Installing Cypress and writing your first passing test is a breeze. There are no servers, drivers, or other dependencies to install or configure."
+            href="https://www.cypress.io/app#delightful_experience"
             src={writeSrc}
           />
           <ProgressItem
-            heading={'Debug failures directly in the browser'}
-            text={'Because Cypress runs directly in the browser, you can debug failed tests using the in-browser developer tools you already know and love.'}
-            href={'https://www.cypress.io/app#visual_debugging'}
+            heading="Debug failures directly in the browser"
+            text="Because Cypress runs directly in the browser, you can debug failed tests using the in-browser developer tools you already know and love."
+            href="https://www.cypress.io/app#visual_debugging"
             src={debugSrc}
           />
           <ProgressItem
-            heading={'Eliminate flaky tests with ease'}
-            text={'Cypress deterministically interacts with your application the same way as your users so you can discover intermittent bugs before your users do.'}
-            href={'https://www.cypress.io/app#flake_resistance'}
+            heading="Eliminate flaky tests with ease"
+            text="Cypress deterministically interacts with your application the same way as your users so you can discover intermittent bugs before your users do."
+            href="https://www.cypress.io/app#flake_resistance"
             src={eliminateSrc}
           />
+          <ProgressItem
+            heading="Integrate Cypress with any CI provider"
+            text="Easily integrate Cypress with your current CI provider. Run Cypress in CI and you will know as soon as there is a failure. We make it simple to run in CI. Use our Docker images or bring your own."
+            href="https://www.cypress.io/cloud#integrated_workflows"
+            src={[int_1, int_2, int_3, int_4, int_5, int_6]}
+            type="special"
+          />
+          <ProgressItem
+            type="break"
+          >
+            <div className={css['heading']}>
+              <ClockSVG className={css['heading__icon']}/>
+              <p className="para para--b para--medium">Introducing Cypress Cloud</p>
+              <h2 className="head head--b head--medium">Increase your productivity and <span className="grad-text grad-text--a">confidence</span></h2>
+            </div>
+          </ProgressItem>
+          <ProgressItem
+            heading="Optimize your runs for a faster feedback loop"
+            text="Run Cypress in your existing CI pipeline and use test parallelization, load balancing, spec prioritization, and more to be as efficient as possible with your available CI resources."
+            href="https://www.cypress.io/cloud#smart_orchestration"
+            src={optimizeSrc}
+          />
+          <ProgressItem
+            heading="Review and debug failures visually"
+            text="Reach new levels of visibility into why your tests failed in CI. Play back videos of your tests as they failed, read friendly stack trace errors, and never guess why another test failed."
+            href="https://www.cypress.io/cloud#visual_reviews"
+            src={reviewSrc}
+          />
+          <ProgressItem
+            heading="Gain actionable insights into your test suite"
+            text="Monitor your test suite’s health with in-depth analytics. Cypress surfaces failing and flaky test result trends and config changes that affect your test suite’s performance."
+            href="https://www.cypress.io/cloud#test_analytics"
+            src={gainSrc}
+          />
+          <ProgressItem
+            heading="Integrate seamlessly into your workflow"
+            text="Plug Cypress into any CI pipeline and you can manage test results as a team thanks to native integrations with Slack, GitHub, GitLab, JIRA, and more."
+            href="https://www.cypress.io/cloud#integrated_workflows"
+            src={integrateSrc}
+          />
+        </div>
+
+        <div className={css['heading']}>
+          <h2 className="head head--b head--medium">Loved by <span className="grad-text grad-text--b">OSS</span>, trusted by <span className="grad-text grad-text--a">Enterprise</span></h2>
+          <p className="para para--b para--big para--weight-300">Cypress is proud to support developers all around the world by making it easier to build and test modern applications.</p>
         </div>
       </div>
     </div>
