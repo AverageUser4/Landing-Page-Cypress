cd45fbb71d5a65851e2eb4caa27e59429d0f3813
kacper <kacpersloka@o2.pl>
2023-05-04 19:31:20 +0200

    real stuff

--- src/components/BigLines/BigLines.js
+++ src/components/BigLines/BigLines.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import css from './BigLines.module.css';
 
-import BigLinesSVG from '../BigLinesSVG/BigLinesSVG';
-
 function BigLines() {
   return (
     <div className={css['container']}>
-      <BigLinesSVG/>
-      <div className={css['heading']}>
+      <div className={css['text']}>
         <h2 className="head head--b head--medium">Loved by <span className="grad-text grad-text--b">OSS</span>, trusted by <span className="grad-text grad-text--a">Enterprise</span></h2>
         <p className="para para--b para--big para--weight-300">Cypress is proud to support developers all around the world by making it easier to build and test modern applications.</p>
       </div>
