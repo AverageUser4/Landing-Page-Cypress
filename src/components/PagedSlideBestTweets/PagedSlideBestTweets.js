7b70f66dd2f6399ffcda0807bed35e8d723872cd
kacper <kacpersloka@o2.pl>
2023-06-10 08:43:01 +0200

    lazy fix to TwitterCards not fitting into the viewport on smaller screen sizes

--- src/components/PagedSlideBestTweets/PagedSlideBestTweets.js
+++ src/components/PagedSlideBestTweets/PagedSlideBestTweets.js
@@ -2,10 +2,13 @@ import React from 'react';
 
 import PagedSlide from '../PagedSlide/PagedSlide';
 import TwitterCard from '../TwitterCard/TwitterCard';
+import { useViewportContext } from '../../context/Viewport';
 
 import guySrc from './assets/guy.webp';
 
 function PagedSlideBestTweets() {
+  const { viewportWidth } = useViewportContext();
+  
   return (
     <PagedSlide>
       <TwitterCard
@@ -16,23 +19,29 @@ function PagedSlideBestTweets() {
         postHref="https://twitter.com/hakimel/status/1583369424845041664"
         color="teal"
       />
-      <TwitterCard
-        avatarSrc={guySrc}
-        username="Hakim El Hattab"
-        profileTag="hakimel"
-        content="Cypress makes me feel about 10x as confident in my work. Just look at those tests go 🥰"
-        postHref="https://twitter.com/hakimel/status/1583369424845041664"
-        color="indigo"
-        isBig={true}
-      />
-      <TwitterCard
-        avatarSrc={guySrc}
-        username="Hakim El Hattab"
-        profileTag="hakimel"
-        content="Cypress makes me feel about 10x as confident in my work. Just look at those tests go 🥰"
-        postHref="https://twitter.com/hakimel/status/1583369424845041664"
-        color="orange"
-      />
+      {
+        viewportWidth >= 600 &&
+          <TwitterCard
+            avatarSrc={guySrc}
+            username="Hakim El Hattab"
+            profileTag="hakimel"
+            content="Cypress makes me feel about 10x as confident in my work. Just look at those tests go 🥰"
+            postHref="https://twitter.com/hakimel/status/1583369424845041664"
+            color="indigo"
+            isBig={true}
+          />
+      }
+      {
+        viewportWidth >= 1000 &&
+          <TwitterCard
+            avatarSrc={guySrc}
+            username="Hakim El Hattab"
+            profileTag="hakimel"
+            content="Cypress makes me feel about 10x as confident in my work. Just look at those tests go 🥰"
+            postHref="https://twitter.com/hakimel/status/1583369424845041664"
+            color="orange"
+          />
+      }
     </PagedSlide>
   );
 }
